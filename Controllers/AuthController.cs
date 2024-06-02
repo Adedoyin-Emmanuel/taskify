410416b54b23c950c92e66bbd2720b2bdd39e3e1
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-06-02 23:48:32 +0100

    feat(app/views): finished login validation

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -17,20 +17,49 @@ namespace taskify.Controllers
             _logger = logger;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
             return View("Login");
         }
 
 
+        [HttpGet]
         public IActionResult Login()
         {
             return View("Login");
         }
 
 
+        [HttpGet]
         public IActionResult Signup()
         {
+            return View("Signup");
+        }
+
+
+        [HttpPost]
+        public IActionResult Signup(SignupViewModel model)
+        {
+
+            Console.WriteLine(model);
+
+            if (!ModelState.IsValid)
+            {
+                return View("Signup", model);
+            }
+
+            Console.WriteLine(HttpContext.Request.Form["Fullname"]);
+            Console.WriteLine(HttpContext.Request.Form["Email"]);
+            Console.WriteLine(HttpContext.Request.Form["Password"]);
+
+
+            Console.WriteLine("This is seperate ---------");
+
+
+
+            Console.WriteLine($"{model.Fullname}, {model.Email}, {model.Password}");
+
             return View("Signup");
         }
 
--- Models/AuthModel.cs
+++ Models/AuthModel.cs
@@ -4,6 +4,7 @@ public class Auth
 {
 
     public required Guid Id { get; set; }
+    public required string Fullname { get; set; }
     public required string Email { get; set; }
     public required string Password { get; set; }
 
