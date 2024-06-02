d656c19c136427ac45a61e39fe0d7454d946a033
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-06-02 19:02:33 +0100

    feat(db/docker): finished docker image

--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
+
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
 
 
 var app = builder.Build();
